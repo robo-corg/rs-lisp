5b54935c52180ac419c9d17383f8c9afe8ffc3ba
Andrew McHarg <amcharg@gmail.com>
2015-02-02 17:03:03 -0800

    Working on getting peeked tokens copied

--- src/parser.rs
+++ src/parser.rs
@@ -15,10 +15,10 @@ impl<'x> Expr for SExpr<'x> {
     
 }
 
-fn parse_s_expr<'a, I>(iter: I) -> SExpr<'a> where I: Iterator<Item=Token>{
+fn parse_s_expr<'a, 'b, I>(iter: &I) -> Box<Expr + 'a> where I: Iterator<Item=(&'b Token)>{
     let mut elements = Vec::new();
 
-    return SExpr { elements: elements };
+    return Box::new(SExpr { elements: elements });
 }
 
 fn parse<'a, 'b, I>(iter: I) -> Vec<Box<Expr + 'a>> where I: Iterator<Item=(&'b Token)>{
@@ -26,11 +26,21 @@ fn parse<'a, 'b, I>(iter: I) -> Vec<Box<Expr + 'a>> where I: Iterator<Item=(&'b 
     let mut exprs: Vec<Box<Expr>> = Vec::new();
 
     loop {
-        match piter.peek() {
+        let tokenResult:Option<Token> = match piter.peek() {
             Some(token) => {
-                match **token {
-                    Token::OpenParen => {}, //exprs.push(Box::new(parse_s_expr(iter))),
-                    _ => {}
+                let borrowedToken:&Token = token;
+                Some(*borrowedToken.clone())
+            },
+            None => None
+        };
+
+        match tokenResult {
+            Some(token) => {
+                match token {
+                    Token::OpenParen => exprs.push(parse_s_expr(&piter)),
+                    _ => {
+                        piter.next();
+                    }
                 }
             },
             None => { break }
@@ -47,9 +57,11 @@ fn parse<'a, 'b, I>(iter: I) -> Vec<Box<Expr + 'a>> where I: Iterator<Item=(&'b 
 
 #[test]
 fn test_parse_empty_s_expr() {
+    let expected:Box<Expr> = Box::new(SExpr {elements: vec!()});
+
     assert_eq!(
         parse(vec!(Token::OpenParen, Token::CloseParen).iter()),
-        vec!(Box::new(SExpr {elements: vec!()}))
+        vec!(expected)
     );
 
 }
